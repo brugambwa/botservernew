f98c7fa903a8784eedafb1cedb57149dc77fc509
104 <brugambwa@gmail.com>
2017-03-17 00:57:20 +0200

    All Fixes

--- controllers/savingscontroller.js
+++ controllers/savingscontroller.js
@@ -40,7 +40,7 @@ function processRequest(req, res, next) {
         res.json(balanceinfo);
       });
   } else if(fxn_call ==='checklimit'){
-      loanCheckAmout(data, function(loanlimitinfo){
+      loanLimitAmoutCheck(data, function(loanlimitinfo){
         res.json(loanlimitinfo);
       });
   } else {
@@ -50,54 +50,89 @@ function processRequest(req, res, next) {
   }
 }
 
-function registerUser(data, callback){
-  var member = new saveModel({
-    fbID: data.fb_id,
-    fName: data.fb_first_name,
-    lName: data.fb_last_name,
-    telephone: data.user_number,
-    accountBalance: 0,
-    loanLimit: 0,
-    loanBalance: 0
+function loanLimitAmoutCheck(data, callback){
+  var uID = data.fb_id;
+  saveModel.find({fbID: uID}, function(err, member) {
+    if (err) {
+      console.log('Could Not Find Any Records.');
+    } else {
+      var messageData = {
+          "messages": [
+            {"text": "You are currently allowed up to "+member[0].loanLimit+" on InstaLoan."}
+          ]
+        };
+        console.log(messageData);
+        callback(messageData);
+    }
   });
+}
 
-  member.save()
-    .then(function(savedUser){
+function checkBalance(data, callback){
+  var uID = data.fb_id;
+  saveModel.findOne({fbID: uID}, function(err, member) {
+    if (err) {
+      console.log('Could Not Find Any Records.');
+    } else {
+      if(!member){
         var messageData = {
-            "messages": [
-              {"text": "Your account has been successfully created. Your Account ID is "+savedUser.id}
-            ]
-          };
-          console.log(messageData)
-          callback(messageData);
-    })
-    .catch(e => console.log(e));
+          "messages": [
+            {"text": "No account record found. Click Join to start saving!"}
+          ]
+        };
+      } else {
+        var messageData = {
+          "messages": [
+            {"text": "Your account balance is  "+member[0].accountBalance+" And your outstanding loan balance is "+member[0].loanBalance}
+          ]
+        };
+      }
+      callback(messageData);
+    }
+  });
+}
+
+function registerUser(data, callback){
+  var uID = data.fb_id;
+  saveModel.findOne({fbID: uID}, function(err, member) {
+    if (err) {
+      console.log('Could Not Find Any Records.');
+    } else {
+      if(!member){
+        var member = new saveModel({
+        fbID: data.fb_id,
+        fName: data.fb_first_name,
+        lName: data.fb_last_name,
+        telephone: data.user_number,
+        accountBalance: 0,
+        loanLimit: 0,
+        loanBalance: 0
+      });
+
+      member.save()
+        .then(function(savedUser){
+            var messageData = {
+                "messages": [
+                  {"text": "Your account has been successfully created. Your Account ID is "+savedUser.id}
+                ]
+              };
+        })
+        .catch(e => console.log(e));
+      } else {
+        var messageData = {
+          "messages": [
+            {"text": "You already have an account. Please select another transaction option"}
+          ]
+        };
+      }
+      callback(messageData);
+    }
+  });
 }
 
 function makeDeposit(data, callback){
 
 }
 
-function checkBalance(data, callback){
-  var uID = data.fb_id;
-  saveModel.find({fbID: uID}, function(err, member) {
-    if (err) {
-      console.log('Could Not Find Any Records.');
-    } else {
-        var messageData = {
-            "messages": [
-              {"text": "Your account balance is  "+member.accountBalance}
-            ]
-          };
-          console.log(messageData)
-          callback(messageData);
-    }
-  });
-}
-
-function loanCheckAmout(data, callback){
-
-}
 
 function makeLoanRequest(data, callback){
   var messageData = {
